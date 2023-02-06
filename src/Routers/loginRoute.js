fba51b5d1d273352138aa8c7ff925efb20c201b8
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-02-06 19:05:38 -0300

    Execute: Usa sistema de validação para informações de email e senha solicitadas no POST da rota /login

--- src/Routers/loginRoute.js
+++ src/Routers/loginRoute.js
@@ -1,16 +1,17 @@
 const express = require('express');
-// const fs = require('fs/promises');
 const crypto = require('crypto');
+const validateEmail = require('../middleware/validateEmail');
+const validatePassword = require('../middleware/validatePassword');
 
 const router = express.Router();
 
 function getToken() {
   return crypto.randomBytes(8).toString('hex');
 }
+// pesquisa para função getToken https://www.tabnine.com/code/javascript/functions/crypto/randomBytes
 
-router.post('/', async (req, res) => {
+router.post('/', validateEmail, validatePassword, (_req, res) => {
   const token = getToken();
-  // const { email, password } = req.body;
 
   return res.status(200).json({ token });
 });
--- src/middleware/validateEmail.js
+++ src/middleware/validateEmail.js
@@ -1,13 +1,14 @@
 const validateEmail = (req, res, next) => {
   const { email } = req.body;
-  const rightEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+  const rightEmail = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/i;
+  // Regex retirado de: https://www.regular-expressions.info/email.html
 
   if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
   if (!rightEmail.test(email)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  }
+  } 
   return next();
 };
 
